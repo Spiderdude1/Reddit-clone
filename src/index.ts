5d48a32d61671555f51b3f3725aa97a793277398
Spiderdude1 <zakiahmed1422@live.com>
2022-03-18 03:16:24 -0700

    GrpahQL enviroment set up

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import {MikroORM} from "@mikro-orm/core";
 import { _prod_ } from "./constants";
-import { Post } from "./entities/Post";
+// /import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello";
 const main = async () => {
 const orm  = await MikroORM.init(microConfig);
 await orm.getMigrator().up();
+const app = express();
+
+
+const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver] , 
+      validate: false
+    }),
+});
+
+apolloServer.applyMiddleware({app});
+
+app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+})
 // const post = orm.em.fork({}).create(Post, {name: "My first post"});
 // orm.em.persistAndFlush(post);
 
-const posts = await orm.em.find(Post, {});
-console.log(posts);
+
 };
 
 main().catch((err) => {
